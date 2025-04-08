79c234eefac503eb0dfdbecd53f03f363c3e4025
sivan <sivanzargari120@gmail.com>
2025-04-08 12:31:10 +0300

    העלאת פרויקט עם React ו-Node.js מחובר לפיירבייס

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/users')
+      .then(response => {
+        // כעת כל מסמך מקבל את ה-ID שלו
+        const usersWithId = response.data.map((user, index) => ({
+          id: index, // תוכל לשים כאן את ה-ID של Firebase אם יש לך
+          ...user,
+        }));
+        setUsers(usersWithId);
+      })
+      .catch(error => {
+        console.error("שגיאה בשליפת הנתונים:", error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>רשימת משתמשים</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>
+            {user.name} - {user.email}  {user.age}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
